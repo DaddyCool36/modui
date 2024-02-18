412fd54265a0c9be12a7dffa6f3904df5b3f3315
Froum <djfroum@gmail.com>
2024-02-18 20:54:53 +0100

    Meilleure visu sur une big grille

--- js/modui.js
+++ js/modui.js
@@ -4,7 +4,7 @@
 */
 
 canvasManager = new CanvasManager("#modui");
-var grille = canvasManager.genererGrille(10, 8);
+var grille = canvasManager.genererGrille(15, 12);
 //var grille = canvasManager.placerAleatoirement(5);
 
 console.log(grille);
@@ -14,63 +14,63 @@ var valH = Math.floor(Math.random() * 360);
 Faker.fakeBouton();
 
 grille.forEach(y => {
-        var can = document.querySelector('#' + y);
-        var valS = Math.floor(40 + Math.random() * 60);
-        var valL = Math.floor(30 + Math.random() * 70);
-        var color = 'hsl(' +
-            valH +
-            'deg, ' +
-            valS +
-            '%, ' +
-            valL +
-            '%)';
-        var epaisseur = Math.round(1 + (Math.random() * 5));
-        var vitesse = 1 + (Math.random() * 4);
+    var can = document.querySelector('#' + y);
+    var valS = Math.floor(40 + Math.random() * 60);
+    var valL = Math.floor(30 + Math.random() * 70);
+    var color = 'hsl(' +
+        valH +
+        'deg, ' +
+        valS +
+        '%, ' +
+        valL +
+        '%)';
+    var epaisseur = Math.round(1 + (Math.random() * 5));
+    var vitesse = 1 + (Math.random() * 4);
 
-        var espacement = Math.round(10 + (Math.random() * 40));
+    var espacement = Math.round(10 + (Math.random() * 40));
 
-        var typesUI = [
+    var typesUI = [
 
-            {
-                'type': 'Fenetre',
-                'params': [
-                    'can',
-                    'vitesse',
-                    'color',
-                    'null',
-                    'null',
-                    'epaisseur'
-                ]
-            },
+        {
+            'type': 'Fenetre',
+            'params': [
+                'can',
+                'vitesse',
+                'color',
+                'null',
+                'null',
+                'epaisseur'
+            ]
+        },
 
-            {
-                'type': 'Grille',
-                'params': [
-                    'can',
-                    'color',
-                    'espacement'
-                ]
-            },
+        {
+            'type': 'Grille',
+            'params': [
+                'can',
+                'color',
+                'espacement'
+            ]
+        },
 
-            {
-                'type': 'Bouton1',
-                'params': [
-                    'can',
-                    'color'
-                ]
-            }
-        ];
+        {
+            'type': 'Bouton1',
+            'params': [
+                'can',
+                'color'
+            ]
+        }
+    ];
 
-        var numUIAleatoire = Math.floor(Math.random() * typesUI.length);
-        var uiType = typesUI[numUIAleatoire];
+    var numUIAleatoire = Math.floor(Math.random() * typesUI.length);
+    var uiType = typesUI[numUIAleatoire];
 
-        var params = uiType.params.join(',');
-        var ui = eval('new ' + uiType.type + '(' + params + ');');
+    var params = uiType.params.join(',');
+    var ui = eval('new ' + uiType.type + '(' + params + ');');
 
-        var ombrageOuiNon = Math.floor(Math.random() * 2);
-        ui.activerOmbre(ombrageOuiNon > 0);
+    var ombrageOuiNon = Math.floor(Math.random() * 2);
+    ui.activerOmbre(ombrageOuiNon > 0);
 
-        animationAleatoire(ui);
+    animationAleatoire(ui);
     
 });
 
